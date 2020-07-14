eababa5038201d6ef6460f21a0b49f78690ca993
amine moumni <childeroe12@gmail.com>
2020-07-14 21:04:25 +0100

    CRUD category

--- app/Category.php
+++ app/Category.php
@@ -7,7 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     
-
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+    
     public function questions()
     {
         return $this->hasMany('App\Question');
--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Category;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class CategoryController extends Controller
 {
@@ -14,7 +16,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        return Category::latest()->get();
     }
 
     /**
@@ -35,7 +37,13 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //Category::create($request->all());
+        $category = new Category();
+        $category->name = $request->name;
+        $category->slug = Str::slug($request->name);
+        $category->save();
+
+        return response('created', Response::HTTP_CREATED);
     }
 
     /**
@@ -46,7 +54,7 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-        //
+        return $category;
     }
 
     /**
@@ -55,9 +63,13 @@ class CategoryController extends Controller
      * @param  \App\Category  $category
      * @return \Illuminate\Http\Response
      */
-    public function edit(Category $category)
+    public function edit(Request $request,Category $category)
     {
-        //
+        $category->update([
+            'name' => $request->name,
+            'slug' => str_slug($request->name)
+        ]);
+        return response(Response::HTTP_ACCEPTED);
     }
 
     /**
@@ -80,6 +92,8 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+
+        return response(Response::HTTP_NO_CONTENT);
     }
 }
